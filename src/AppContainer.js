2ce97d41fe1321f0f792c2d0d3837d6f47ba0012
mandriv <wojciech.cichoradzki@gmail.com>
2019-11-10 20:03:39 +0000

    Add some placeholder screens

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -11,6 +11,7 @@ const AppNavigator = createStackNavigator({
   SignIn: {
     screen: SignIn,
   },
+  initialRouteName: 'Welcome',
 });
 
 export default createAppContainer(AppNavigator);
--- src/screens/Welcome/Welcome.js
+++ src/screens/Welcome/Welcome.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Button} from 'react-native';
 
-const Welcome = () => {
+const Welcome = ({navigation}) => {
   return (
     <SafeAreaView>
       <View>
         <Text>Welcome Screen</Text>
+        <Button
+          title="Let's start"
+          onPress={() => navigation.navigate('SignIn')}
+        />
       </View>
     </SafeAreaView>
   );
