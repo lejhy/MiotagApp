69f42303a19edcaf67c1697271e74c05884ed1f1
mandriv <wojciech.cichoradzki@gmail.com>
2019-11-29 08:18:32 +0000

    Add icons

--- src/screens/App/Dashboard/Dashboard.jsx
+++ src/screens/App/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { Text } from '@core';
 
@@ -17,6 +18,12 @@ export default function Dashboard(props: Props) {
   return (
     <Container>
       <Text>Dashboard</Text>
+      <Text>Dashboard</Text>
+      <Text>Dashboard</Text>
+      <Text>Dashboard</Text>
+      <Text>Dashboard</Text>
+      <Text>Dashboard</Text>
+      <Icon name="rocket" size={30} color="#900" />
     </Container>
   );
 }
