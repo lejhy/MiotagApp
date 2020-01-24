540329173bf9c90fb4d5e4c54f3a3246f231c05f
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2020-01-24 03:27:58 +0000

    Working example with expo-three

--- src/screens/App/Activities/FreeMode/FreeMode.jsx
+++ src/screens/App/Activities/FreeMode/FreeMode.jsx
@@ -1,41 +1,19 @@
 // @flow
 
-import React, { PureComponent } from "react";
-import { AppRegistry, StyleSheet, StatusBar } from "react-native";
-import { GameEngine } from "react-native-game-engine";
-import { Finger } from "./Renderers";
-import { MoveFinger } from "./Systems"
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import { Hand } from './Renderers';
 
-export default class FreeMode extends PureComponent {
+export default class FreeMode extends Component {
   constructor() {
     super();
   }
 
   render() {
-    return (
-      <GameEngine
-        style={styles.container}
-        systems={[MoveFinger]}
-        entities={{
-          0: { position: [40,  200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
-          1: { position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
-          2: { position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
-          3: { position: [220, 200], renderer: <Finger />},
-          4: { position: [280, 200], renderer: <Finger />}
-        }}>
-
-        <StatusBar hidden={true} />
-
-      </GameEngine>
-    );
+    return(
+      <Hand />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF"
-  }
-});
 
 AppRegistry.registerComponent("FreeMode", () => FreeMode);
