966e42feaf1d76a2fe15f3b0f35cb144f8cd9ed4
mandriv <wojciech.cichoradzki@gmail.com>
2020-01-29 01:25:21 +0000

    Filter myself from user search

--- src/hooks/useUserSearch/index.js
+++ src/hooks/useUserSearch/index.js
@@ -4,15 +4,18 @@ import { useState, useEffect } from 'react';
 
 import UserService from '@services/api/UserService';
 
+import useUser from '../useUser';
+
 export const STORAGE_KEY = 'friends_cache';
 
 export default function useUserSearch() {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [{ id }] = useUser();
 
   const search = async () => {
     const searchResponse = await UserService.search(query);
-    setResults(searchResponse.data);
+    setResults(searchResponse.data.filter((u) => u.id !== id));
   };
 
 
