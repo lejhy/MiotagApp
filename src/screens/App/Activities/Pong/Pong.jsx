24e5bfeb40a5791a84ad916902e7033d1948f2e1
mandriv <wojciech.cichoradzki@gmail.com>
2020-02-19 03:53:02 +0000

    Add Pong, disable navigation gestures for games

---
+++ src/screens/App/Activities/Pong/Pong.jsx
@@ -0,0 +1,20 @@
+// @flow
+
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.View``;
+
+const Text = styled.Text``;
+
+type Props = {
+
+};
+
+export default function Pong(props: Props) {
+  return (
+    <Container>
+      <Text>Pong</Text>
+    </Container>
+  );
+}
---
+++ src/screens/App/Activities/Pong/index.js
@@ -0,0 +1,5 @@
+// @flow
+
+import Pong from './Pong';
+
+export default Pong;
