c413b4d83ce3d6e8f1b95fdfcf648c794ea2f170
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2020-02-20 21:08:13 +0000

    More sensors

--- src/hooks/useMiotag/sensors.js
+++ src/hooks/useMiotag/sensors.js
@@ -8,6 +8,15 @@ const ThumbUUID = '19B10006-E8F2-537E-4F6C-D104768A1214';
 const RollUUID = '19B10007-E8F2-537E-4F6C-D104768A1214';
 const PitchUUID = '19B10008-E8F2-537E-4F6C-D104768A1214';
 const YawUUID = '19B10009-E8F2-537E-4F6C-D104768A1214';
+const AccXUUID = '19B10010-E8F2-537E-4F6C-D104768A1214';
+const AccYUUID = '19B10011-E8F2-537E-4F6C-D104768A1214';
+const AccZUUID = '19B10012-E8F2-537E-4F6C-D104768A1214';
+const GyroXUUID = '19B10013-E8F2-537E-4F6C-D104768A1214';
+const GyroYUUID = '19B10014-E8F2-537E-4F6C-D104768A1214';
+const GyroZUUID = '19B10015-E8F2-537E-4F6C-D104768A1214';
+const MagXUUID = '19B10016-E8F2-537E-4F6C-D104768A1214';
+const MagYUUID = '19B10017-E8F2-537E-4F6C-D104768A1214';
+const MagZUUID = '19B10018-E8F2-537E-4F6C-D104768A1214';
 
 const SENSORS = {
   [ThumbUUID]: 'thumb',
@@ -18,6 +27,15 @@ const SENSORS = {
   [RollUUID]: 'roll',
   [PitchUUID]: 'pitch',
   [YawUUID]: 'yaw',
+  [AccXUUID]: 'accX',
+  [AccYUUID]: 'accY',
+  [AccZUUID]: 'accZ',
+  [GyroXUUID]: 'gyroX',
+  [GyroYUUID]: 'gyroY',
+  [GyroZUUID]: 'gyroZ',
+  [MagXUUID]: 'magX',
+  [MagYUUID]: 'magY',
+  [MagZUUID]: 'magZ'
 };
 
 const reducer = (state, action) => {
@@ -86,6 +104,78 @@ const reducer = (state, action) => {
           yaw: action.value,
         },
       };
+    case SENSORS[AccXUUID]:
+      return {
+        ...state,
+        acc: {
+          ...state.acc,
+          x: action.value,
+        },
+      };
+    case SENSORS[AccYUUID]:
+      return {
+        ...state,
+        acc: {
+          ...state.acc,
+          y: action.value,
+        },
+      };
+    case SENSORS[AccZUUID]:
+      return {
+        ...state,
+        acc: {
+          ...state.acc,
+          z: action.value,
+        },
+      };
+    case SENSORS[GyroXUUID]:
+      return {
+        ...state,
+        gyro: {
+          ...state.gyro,
+          x: action.value,
+        },
+      };
+    case SENSORS[GyroYUUID]:
+      return {
+        ...state,
+        gyro: {
+          ...state.gyro,
+          y: action.value,
+        },
+      };
+    case SENSORS[GyroZUUID]:
+      return {
+        ...state,
+        gyro: {
+          ...state.gyro,
+          z: action.value,
+        },
+      };
+    case SENSORS[MagXUUID]:
+      return {
+        ...state,
+        mag: {
+          ...state.mag,
+          x: action.value,
+        },
+      };
+    case SENSORS[MagYUUID]:
+      return {
+        ...state,
+        mag: {
+          ...state.mag,
+          y: action.value,
+        },
+      };
+    case SENSORS[MagZUUID]:
+      return {
+        ...state,
+        mag: {
+          ...state.mag,
+          z: action.value,
+        },
+      };
     default:
       return { ...state };
   }
@@ -103,6 +193,21 @@ const initialState = {
     roll: null,
     pitch: null,
     yaw: null,
+  },
+  acc: {
+    x: null,
+    y: null,
+    z: null
+  },
+  gyro: {
+    x: null,
+    y: null,
+    z: null
+  },
+  mag: {
+    x: null,
+    y: null,
+    z: null
   },
 };
 
--- src/screens/App/Activities/Debug/Debug.jsx
+++ src/screens/App/Activities/Debug/Debug.jsx
@@ -14,13 +14,25 @@ type Props = {
   sensors: Sensors,
 }
 
-const Debug = ({ miotag: { sensors: { axes } } }: Props) => (
+const Debug = ({ miotag: { sensors: { axes, acc, gyro, mag } } }: Props) => (
   <SafeAreaView>
     <Text>Debug screen</Text>
     <Text>Axes:</Text>
     <Text>{ `Roll: ${axes.roll === null ? 'null' : axes.roll}` }</Text>
     <Text>{ `Pitch: ${axes.pitch === null ? 'null' : axes.pitch}` }</Text>
     <Text>{ `Yaw: ${axes.yaw === null ? 'null' : axes.yaw}` }</Text>
+    <Text>Acc:</Text>
+    <Text>{ `X: ${acc.x === null ? 'null' : acc.x}` }</Text>
+    <Text>{ `Y: ${acc.y === null ? 'null' : acc.y}` }</Text>
+    <Text>{ `Z: ${acc.z === null ? 'null' : acc.z}` }</Text>
+    <Text>Gyro:</Text>
+    <Text>{ `X: ${gyro.x === null ? 'null' : gyro.x}` }</Text>
+    <Text>{ `Y: ${gyro.y === null ? 'null' : gyro.y}` }</Text>
+    <Text>{ `Z: ${gyro.z === null ? 'null' : gyro.z}` }</Text>
+    <Text>Mag:</Text>
+    <Text>{ `X: ${mag.x === null ? 'null' : mag.x}` }</Text>
+    <Text>{ `Y: ${mag.y === null ? 'null' : mag.y}` }</Text>
+    <Text>{ `Z: ${mag.z === null ? 'null' : mag.z}` }</Text>
   </SafeAreaView>
 );
 
