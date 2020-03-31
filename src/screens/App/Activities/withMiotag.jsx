8ce1099f4b28339a7ca160dcc4c56d33d9ea2ad6
mandriv <wojciech.cichoradzki@gmail.com>
2020-03-31 16:00:17 +0100

    Lock games to portrait optionally, keep screen awake in games

--- src/screens/App/Activities/withMiotag.jsx
+++ src/screens/App/Activities/withMiotag.jsx
@@ -1,8 +1,10 @@
 // @flow
 
-import React, {useContext, useRef} from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ActivityIndicator } from 'react-native';
 import styled, { ThemeContext } from 'styled-components';
+import { useKeepAwake } from 'expo-keep-awake';
+import Orientation from 'react-native-orientation-locker';
 
 import useMiotag from '@hooks/useMiotag/hook';
 import useUser from '@hooks/useUser';
@@ -20,21 +22,29 @@ const withMiotag = (staticParams) => (Game) => (props) => {
   const [{ gameDebug }] = useUser();
   const {
     getImu, getFingers, getQuaternions, isAvailable,
-  } = useMiotag();
-  const {getPhoneImu} = usePhoneSensors();
+  } = useMiotag(!gameDebug);
+  const { getPhoneImu } = usePhoneSensors();
   const theme = useContext(ThemeContext);
+  useKeepAwake();
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  if (gameDebug) return (
-    <Game
-      getImu={getPhoneImu}
-      getFingers={() => new Uint8Array(5)}
-      getQuaternions={() => new Float32Array(4)}
-      {...props}
-    />
-  );
+  const { blockView = true, lockToPortrait = false } = staticParams || {};
 
-  const { blockView = true } = staticParams || {};
+  useEffect(() => {
+    if (lockToPortrait) {
+      Orientation.lockToPortrait();
+    }
+  }, []);
+
+  if (gameDebug) {
+    return (
+      <Game
+        getImu={getPhoneImu}
+        getFingers={() => new Uint8Array(5)}
+        getQuaternions={() => new Float32Array(4)}
+        {...props}
+      />
+    );
+  }
 
   if (!isAvailable && blockView) {
     return (
