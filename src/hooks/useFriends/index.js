64cafd217e9f9b5c85c76cee9c2b3851b841f117
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2020-03-31 23:06:43 +0100

    Added last name to friend ordering

--- src/hooks/useFriends/index.js
+++ src/hooks/useFriends/index.js
@@ -22,7 +22,7 @@ export default function useFriends() {
     const followResponse = await RelationService.getUsersFollowed();
 
     const newState = followResponse.data;
-    newState.sort((f1, f2) => f1.firstName > f2.firstName);
+    newState.sort((f1, f2) => f1.firstName === f2.firstName ? f1.lastName > f2.lastName : f1.firstName > f2.firstName);
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newState));
     setState(newState);
     return newState;
