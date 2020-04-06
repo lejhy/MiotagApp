d9bb13eb03ccdcb523ec7ef413857e97214408af
mandriv <wojciech.cichoradzki@gmail.com>
2020-04-06 20:58:28 +0100

    Capitalize app name fix

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,13 +1,8 @@
 // @flow
 
 import axios from 'axios';
-import AsyncStorage from '@react-native-community/async-storage';
 import { API_URL } from 'react-native-dotenv';
 
-
-
-
-import { STORAGE_KEY } from '@hooks/useUser/hook';
 
 const FALLBACK_URL = 'http://lejhanec.com:8080';
 
